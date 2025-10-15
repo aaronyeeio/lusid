cb8a12eb1cfe4efcfa8c5e7273f60d56c9125d6c
aaronyeeio <aaronyee97@gmail.com>
2025-10-15 18:05:50 +0800

    Update default parameter for ignore_stop in send_message function to True

--- src/lusid/send_message.py
+++ src/lusid/send_message.py
@@ -1,6 +1,6 @@
 from .message_client import MessageClient
 
 
-async def send_message(to, body, ignore_stop=False, use_applescript=True):
+async def send_message(to, body, ignore_stop=True, use_applescript=True):
     mc = MessageClient()
     await mc.send_message(to, body, ignore_stop, use_applescript)
